b17ce5278f236b9a11c86d1d42fae765b1315490
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2024-08-05 15:47:06 -0300

    Fraction impl and tests passing

--- haskell-exercises/fraction/src/Fraction.hs
+++ haskell-exercises/fraction/src/Fraction.hs
@@ -5,27 +5,29 @@ type Fraction = (Int, Int)
 -- Implement the `add` Function
 
 add :: Fraction -> Fraction -> Fraction
-add n d = error "Implement it"
+add (a, b) (c, d) = simplify (a * d + c * b, b * d)
 
 -- Implement the `sub` Function
 
 sub :: Fraction -> Fraction -> Fraction
-sub n d = error "Implement it"
+sub (a, b) (c, d) = simplify (a * d - c * b, b * d)
 
 -- Implement the `mul` Function
 
 mul :: Fraction -> Fraction -> Fraction
-mul n d = error "Implement it"
+mul (a, b) (c, d) = simplify (a * c, b * d)
 
 -- Implement the `divide` Function
 
 divide :: Fraction -> Fraction -> Fraction
-divide n d = error "Implement it"
+divide (a, b) (c, d) = simplify (a * d, b * c)
 
 -- Implement the `hcf` Function
 
 hcf :: Int -> Int -> Int
-hcf n d = error "Implement it"
+hcf a 0 = a
+hcf a b = hcf b (a `mod` b)
 
-
-    
+-- Implement the `simplify` Function
+simplify :: Fraction -> Fraction
+simplify (n, d) = let g = hcf n d in (n `div` g, d `div` g)
