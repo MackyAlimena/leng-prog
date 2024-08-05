e3a3d6de16b3b926f3026419ad5385e6d588fab8
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2024-08-05 15:52:32 -0300

    CollatzConjecture impl and tests passing

--- haskell-exercises/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell-exercises/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,4 +1,19 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz = error "You need to implement this function."
+collatz n
+  | n <= 0    = Nothing
+  | n == 1    = Just 0
+  | even n    = case collatz (n `div` 2) of
+                  Just steps -> Just (steps + 1)
+                  Nothing    -> Nothing
+  | otherwise = case collatz (3 * n + 1) of
+                  Just steps -> Just (steps + 1)
+                  Nothing    -> Nothing
+
+-- or:
+-- collatz n
+--  | n <= 0    = Nothing
+--   | n == 1    = Just 0
+--   | even n    = fmap (+1) (collatz (n `div` 2))
+--   | otherwise = fmap (+1) (collatz (3 * n + 1))
