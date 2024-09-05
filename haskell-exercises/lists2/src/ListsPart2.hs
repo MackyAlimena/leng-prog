bfbe0528befc192d800db66df44fda085676dd63
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2024-09-05 16:53:33 -0300

    exercises 1 and 2 done for Lists2, with tests passing

--- haskell-exercises/lists2/src/ListsPart2.hs
+++ haskell-exercises/lists2/src/ListsPart2.hs
@@ -7,16 +7,13 @@ data Bit = F | T  deriving (Eq, Show, Enum, Read)
 type Bits = [Bit]
 
 bitAt :: Int -> Char -> Bit
-bitAt n c = if testBit (ord c) (7-n) then T else F 
+bitAt n c = if testBit (ord c) (7-n) then T else F
 
 charToBits :: Char -> Bits
-charToBits = error "Implement It"
+charToBits c = [bitAt i c | i <- [0..7]]
 
-
-bits::String -> Bits
-bits = error "Implement It"
+bits :: String -> Bits
+bits = foldr (\c acc -> charToBits c ++ acc) []
 
 type Solution = [Int]
 
-queens::Int -> [Solution]
-queens error "Implement It"
