2d0642ae0b74a7cc55de3b8b31139c8c76fed71e
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2024-09-06 15:30:43 -0300

    binary trie done and tests passing

--- haskell-exercises/trie/src/Trie.hs
+++ haskell-exercises/trie/src/Trie.hs
@@ -1,20 +1,38 @@
-module Trie  (Trie(..), left, right, find, decode, toList) where
+module Trie (Trie(..), left, right, find, decode, toList) where
 
 import Bit
-  
-data Trie a = Complete -- Add the Type definition deriving (Eq, Show)
-            
-left::Trie a -> Trie a
-left = error "Define it"
 
-right::Trie a -> Trie a
-left = error "Define it"
-  
-find::Bits -> Trie a -> a
-find = error "Define it"
+data Trie a = Leaf a | (Trie a) :-: (Trie a) deriving (Eq, Show)
 
-decode::Bits -> Trie Char -> String
-decode = error "Define it"
+left :: Trie a -> Trie a
+left (Leaf _) = error "Left of: Leaf"
+left (a :-: _) = a
 
-toList::Trie a -> [(a, Bits)]
-toList = error "Define it"
+right :: Trie a -> Trie a
+right (Leaf _) = error "Right of: Leaf"
+right (_ :-: a) = a
+
+find :: Bits -> Trie a -> a
+find [] (Leaf a) = a
+find (x:xs) (l :-: r)
+    | x == F = find xs l
+    | x == T = find xs r
+find _ _ = error "Pattern not matched"
+
+decode :: Bits -> Trie Char -> String
+decode bits trie = decodeAux bits trie trie
+
+decodeAux :: Bits -> Trie Char -> Trie Char -> String
+decodeAux [] (Leaf a) _ = [a]
+decodeAux bits (Leaf a) trie = a : decodeAux bits trie trie
+decodeAux (x:xs) (l :-: r) trie
+    | x == F = decodeAux xs l trie
+    | x == T = decodeAux xs r trie
+decodeAux _ _ _ = error "Pattern not matched"
+
+toList :: Trie a -> [(a, Bits)]
+toList t = toListAux t []
+
+toListAux :: Trie a -> Bits -> [(a, Bits)]
+toListAux (Leaf a) bits = [(a, bits)]
+toListAux (l :-: r) bits = toListAux l (bits ++ [F]) ++ toListAux r (bits ++ [T])
--- haskell-exercises/trie/test/Tests.hs
+++ haskell-exercises/trie/test/Tests.hs
@@ -1,37 +1,38 @@
 import Test.Hspec    (describe, hspec, it, shouldBe)
 
 import Trie (Trie(..), left, right, find, decode, toList)
+
 import Bit (Bit(T, F))
 
 main :: IO ()
 main = hspec $ do
-  let trie = (Leaf 'A' :-: Leaf 'B') :-: Leaf 'C'
-  describe "Binary Trie - Basic" $ do
-    it "left" $ do
-      left trie `shouldBe` (Leaf 'A' :-: Leaf 'B')
-      
-    it "right" $ do
-      right trie `shouldBe` Leaf 'C'
+ let trie = (Leaf 'A' :-: Leaf 'B') :-: Leaf 'C'
+ describe "Binary Trie - Basic" $ do
+   it "left" $ do
+     left trie `shouldBe` (Leaf 'A' :-: Leaf 'B')
 
-    it "left, left" $ do
-      left (left trie) `shouldBe` Leaf 'A'
+   it "right" $ do
+     right trie `shouldBe` Leaf 'C'
 
-  describe "find" $ do
-    it "T -> A" $ do
-      [T] `find` trie `shouldBe` 'C'
-      
-    it "FT -> B" $ do
-      [F, T] `find` trie `shouldBe` 'B'
+   it "left, left" $ do
+     left (left trie) `shouldBe` Leaf 'A'
 
-  describe "decode" $ do
-    it "FTFFFTFF -> BABA" $ do
-      [F, T, F, F, F, T, F, F] `decode` trie `shouldBe` "BABA" 
+ describe "find" $ do
+   it "T -> A" $ do
+     [T] `find` trie `shouldBe` 'C'
 
-    it "TFFFTFF -> CABA" $ do
-      [T, F, F, F, T, F, F] `decode` trie `shouldBe` "CABA" 
+   it "FT -> B" $ do
+     [F, T] `find` trie `shouldBe` 'B'
 
-  describe "toList" $ do
-    it "(Leaf 'A' :-: Leaf 'B') :-: Leaf 'C'" $ do
-      toList trie `shouldBe` [('A',[F,F]),('B',[F,T]),('C',[T])]
-    it "Leaf 'A' :-: Leaf 'B'" $ do
-      toList (left trie) `shouldBe` [('A',[F]),('B',[T])]
+ describe "decode" $ do
+   it "FTFFFTFF -> BABA" $ do
+     [F, T, F, F, F, T, F, F] `decode` trie `shouldBe` "BABA"
+
+   it "TFFFTFF -> CABA" $ do
+     [T, F, F, F, T, F, F] `decode` trie `shouldBe` "CABA"
+
+ describe "toList" $ do
+   it "(Leaf 'A' :-: Leaf 'B') :-: Leaf 'C'" $ do
+     toList trie `shouldBe` [('A',[F,F]),('B',[F,T]),('C',[T])]
+   it "Leaf 'A' :-: Leaf 'B'" $ do
+     toList (left trie) `shouldBe` [('A',[F]),('B',[T])]
