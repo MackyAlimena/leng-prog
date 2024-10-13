5881b6195d0ce424c3b55cad913fdceedce1aeb8
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2024-10-13 19:23:45 -0300

    exercise done

--- rust-exercises/collatz-conjecture/src/lib.rs
+++ rust-exercises/collatz-conjecture/src/lib.rs
@@ -1,4 +1,16 @@
 pub fn collatz(mut n: u64) -> Option<u64> {
-  None
+  let mut steps = 0;
+  while n!=1{
+      if n == 0 {
+          return None;
+      } else {
+          match n {
+          _ if n % 2 == 0 => n /= 2,
+          _ => n = 3 * n + 1,
+          }
+          steps += 1;
+      }
+  }
+  Some(steps)
 }
 
