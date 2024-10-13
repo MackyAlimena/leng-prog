b23cb2dca6eaea2e13df8ea1a0249479202ebec5
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2024-10-13 20:58:23 -0300

    done

--- rust-exercises/queen-attack/src/lib.rs
+++ rust-exercises/queen-attack/src/lib.rs
@@ -8,16 +8,37 @@ pub struct Queen {
 
 impl ChessPosition {
     pub fn new(rank: i32, file: i32) -> Option<Self> {
-        panic!("Implement me");
+        for i in 0..8 {
+            if rank == i {
+                for j in 0..8 {
+                    if file == j {
+                        return Some(ChessPosition(rank, file));
+                    }
+                }
+            }
+        } None
     }
+
+    //alternative
+    /*pub fn new(rank: i32, file: i32) -> Option<Self> {
+        if (0..8).contains(&rank) && (0..8).contains(&file) {
+            Some(ChessPosition(rank, file))
+        } else {
+            None
+        }
+    }*/
 }
+
 
 impl Queen {
     pub fn new(position: ChessPosition) -> Self {
-        panic!("Implement me");
+        Queen { position }
     }
 
     pub fn can_attack(&self, other: &Queen) -> bool {
-    panic!("Implement me");
-}
+        let (x1, y1) = (self.position.0, self.position.1);
+        let (x2, y2) = (other.position.0, other.position.1);
+
+        x1 == x2 || y1 == y2 || (x1 - x2).abs() == (y1 - y2).abs()
+    }
 }
