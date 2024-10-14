ee8e7c41d09e1cf8fa20c4f1fd682a6d779dc5ba
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2024-10-13 22:07:37 -0300

    tests passing

--- rust-exercises/sum-of-multiples/src/lib.rs
+++ rust-exercises/sum-of-multiples/src/lib.rs
@@ -1,3 +1,9 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    0
+    let mut sum = 0;
+    for i in 1..limit {
+        if factors.iter().any(|&factor| factor != 0 && i % factor == 0) {
+            sum += i;
+        }
+    }
+    sum
 }
