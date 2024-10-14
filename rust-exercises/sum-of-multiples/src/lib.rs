e4ea5e7b7477e0d8d865da68d0a8e9c8ebd459a6
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2024-10-13 22:12:29 -0300

    adding an alternative impl

--- rust-exercises/sum-of-multiples/src/lib.rs
+++ rust-exercises/sum-of-multiples/src/lib.rs
@@ -7,3 +7,20 @@ pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
     }
     sum
 }
+
+//impl using for_each
+/*pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
+    let mut sum = 0;
+    for i in 1..limit {
+        let mut is_multiple = false;
+        factors.iter().for_each(|&factor| {
+            if factor != 0 && i % factor == 0 {
+                is_multiple = true;
+            }
+        });
+        if is_multiple {
+            sum += i;
+        }
+    }
+    sum
+}*/
