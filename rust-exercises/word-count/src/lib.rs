0ede08cb69e76246276e6041bca1dd49ce6508d3
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2024-10-13 22:32:27 -0300

    adding an alternative impl

--- rust-exercises/word-count/src/lib.rs
+++ rust-exercises/word-count/src/lib.rs
@@ -2,5 +2,25 @@ use std::collections::HashMap;
 
 /// Count occurrences of words.
 pub fn word_count(phrase: &str) -> HashMap<String, u32> {
-  panic!("Implement me!")
+    let mut map = HashMap::new();
+    for word in phrase
+        .to_lowercase()
+        .split_whitespace() {
+        *map.entry(word.to_string()).or_insert(0) += 1;
+    }
+    map
 }
+
+//alternative with fold
+/*use std::collections::HashMap;
+
+/// Count occurrences of words.
+pub fn word_count(phrase: &str) -> HashMap<String, u32> {
+    phrase
+        .to_lowercase()
+        .split_whitespace()
+        .fold(HashMap::new(), |mut map, word| {
+            *map.entry(word.to_string()).or_insert(0) += 1;
+            map
+        })
+}*/
