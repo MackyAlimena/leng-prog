81727d349cf95afb8bcf1b43efce400839c79d9d
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2024-10-13 21:23:14 -0300

    tests passing

--- rust-exercises/clock/src/lib.rs
+++ rust-exercises/clock/src/lib.rs
@@ -8,16 +8,28 @@ pub struct Clock {
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        panic!("Implement me!");
+        let total_minutes = hours * 60 + minutes;
+        let normalized_minutes = total_minutes.rem_euclid(1440);
+        Clock {
+            hours: normalized_minutes / 60,
+            minutes: normalized_minutes % 60,
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        panic!("Implement me!");
+        Clock::new(self.hours, self.minutes + minutes)
     }
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        panic!("Implement me!");
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
     }
 }
+
+//String::from impl for Clock
+/*impl From<Clock> for String {
+    fn from(clock: Clock) -> Self {
+        format!("{:02}:{:02}", clock.hours, clock.minutes)
+    }
+}*/
