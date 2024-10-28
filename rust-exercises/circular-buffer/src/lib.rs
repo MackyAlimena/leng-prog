1f60345040950866687a01738bf7ef1f44a512d1
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2024-10-28 01:37:00 -0300

    tests passing for circular buffer

--- rust-exercises/circular-buffer/src/lib.rs
+++ rust-exercises/circular-buffer/src/lib.rs
@@ -20,22 +20,48 @@ impl<T:Clone> CircularBuffer<T> {
     }
 
     pub fn read(&mut self) -> Result<T, Error> {
-        todo!()
+        if self.is_empty() {
+            return Err(Error:: EmptyBuffer);
+        }else{
+            // Calculate the index of the oldest element
+            let index = (self.buffer.len() + self.next - self.size) % self.buffer.len();
+            // Retrieve the element at the calculated index
+            let result = self.buffer[index].clone();
+            // Decrease the size of the buffer
+            self.size -= 1;
+            match result {
+                Some(res) => Ok(res),
+                None => Err(Error:: EmptyBuffer)
+            }
+        }
     }
 
     pub fn write(&mut self, byte: T) -> Result<(), Error> {
-        todo!()
+        if self.is_full() {
+            return Err(Error:: FullBuffer);
+        }else{
+            // Insert the byte at the next position
+            self.buffer[self.next] = Some(byte);
+            // Update the next position, wrapping around if necessary
+            self.next = (self.next + 1) % self.buffer.len();
+            // Increase the size of the buffer
+            self.size += 1;
+            // Return Ok to indicate success
+            Ok(())
+        }
     }
 
     pub fn clear(&mut self) {
-        todo!()
+        self.size = 0;
+        self.next = 0;
+        self.buffer = vec![None; self.buffer.len()];
     }
 
     pub fn is_empty(&self) -> bool {
-        todo!()
+        return self.size == 0;
     }
 
     pub fn is_full(&self) -> bool {
-        todo!()
+        return self.size == self.buffer.len();
     }
 }
